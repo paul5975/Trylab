377e7df23f5bcd91779b052adc4ea4f3f8b1a77b
paul5975 <paul.luong59@gmail.com>
2016-02-10 12:30:12 +0100

    new

---
+++ main.c
@@ -0,0 +1,28 @@
+//
+//  main.c
+//  Mimi
+//
+//  Created by Paul Luong on 27/01/2016.
+//  Copyright (c) 2016 Paul Luong. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int i;
+    printf("Coucou mimi\n");
+    printf("Comment tu vas \n");
+    printf("1. Tres bien !\n");
+    printf("2. Pas bien ! \n");
+    scanf("%d",&i );
+    if (i==1)
+    {printf("Youpi");};
+    if (i==2)
+    {printf("Oh bah pourquoi pauvre coeur");};
+    
+    
+    
+    
+    return 0;
+}
